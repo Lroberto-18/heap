20188b01c2652fcb821c2f2d5bb435b36e901051
Lroberto-18 <leandroroberto582@gmail.com>
2022-11-14 15:21:42 -0300

    v1.0.6 remove func

--- heap_v.c
+++ heap_v.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void down(int* heap[],int j,int n){
+void down(int heap[],int j,int n){
     /*
     Função para descer elementos do heap.
     heap[] -> heap criado.
-    j -> posição do elemento que irá descer.
+    j -> posição do elemento que irá descer. (0 até n-1)
     n -> numero de elementos do vetor.
     */
     int aux = j*2+1;
@@ -22,7 +22,7 @@ void down(int* heap[],int j,int n){
         }
     }
 }
-void creat(int *heap[],int v[],int n){
+void creat(int heap[],int v[],int n){
     //criar heap com o vetor informado
     for(int i=0;i<n;i++){
         heap[i]=v[i];
@@ -32,11 +32,11 @@ void creat(int *heap[],int v[],int n){
         down(heap,j-1,n);
     }
 }
-void up(int* heap[],int j,int value, int n){
+void up(int heap[],int j,int value, int n){
     /*
     Função para aumentar prioridade elementos do heap.
     heap[] -> heap criado.
-    j -> posição do elemento que irá descer.
+    j -> posição do elemento que irá descer. (0 até n-1)
     value -> novo valor
     n -> numero de elementos do vetor.
     */
@@ -49,20 +49,43 @@ void up(int* heap[],int j,int value, int n){
         up(heap,aux,value,n);
     }
 }
+
+void remove_element(int heap[],int *n){
+    heap[0] = heap[*n-1];
+    heap[*n-1] = 0;
+    *n-=1;
+    down(heap,0,*n-1);
+}
+
 int main(){
     int v[] = {1,2,3,4,5,6,7,8,9,10,11};
     int n= sizeof(v)/sizeof(v[0]);//tamanho do vetor v
-    int *heap[n];
+    printf("t->%d\n",n);
+    int heap[n];
     creat(heap,v,n);  //criando o heap
     printf("\n");
     printf("\no HEAP criado é ( ");
     for(int i=0;i<n;i++){
         printf("%d ",heap[i]);
     }printf(")\n");
-    up(heap,5,100,11);
-    printf("\no HEAP criado é ( ");
+    up(heap,7,177,11);
+    printf("\nHEAP( ");
     for(int i=0;i<n;i++){
         printf("%d ",heap[i]);
     }printf(")\n");
+    
+    remove_element(heap,&n);
+    printf("\nHEAP( ");
+    for(int i=0;i<n;i++){
+        printf("%d ",heap[i]);
+    }printf(")\n");
+    printf("t->%d\n",n);
+    remove_element(heap,&n);
+    printf("\nHEAP( ");
+    for(int i=0;i<n;i++){
+        printf("%d ",heap[i]);
+    }printf(")\n");
+    printf("t->%d\n",n);
+    
 return 0;
 }
