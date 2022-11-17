a17a0d95fbaa0bc5beb3a11c9f4e3fb2df2d20cd
lroberto-18 <leandroroberto582@gmail.com>
2022-11-17 15:20:04 -0300

    v1.0.8 correção no down() e comentários

--- heap_v.c
+++ heap_v.c
@@ -3,19 +3,19 @@
 
 void down(int heap[],int j,int n){
     /*
-    Função para descer elementos do heap.
-    heap[] -> heap criado.
-    j -> posição do elemento que irá descer. (0 até n-1)
-    n -> numero de elementos do vetor.
+    * Função para descer elementos do heap.
+    * heap[] -> heap criado.
+    * j -> posição do elemento que irá descer. (0 até n-1)
+    * n -> numero de elementos do vetor.
     */
     int aux = j*2+1;
     if(aux<=n){
-        if(heap[j]<heap[aux]){
-            if(aux+1<n){
+        if(aux+1<n){
                 if(heap[j]<heap[aux+1]){
                     if(heap[aux]<heap[aux+1]) aux +=1;
                 }
             }
+        if(heap[j]<heap[aux]){
             int swap = heap[j];
             heap[j] = heap[aux];
             heap[aux] = swap;
@@ -23,9 +23,11 @@ void down(int heap[],int j,int n){
         }
     }
 }
-
 void creat(int heap[],int v[],int n){
-    //criar heap com o vetor informado
+    /*
+    * Criar heap com vetor informado.
+    * heap[] -> vetor alocado para ser o heap.
+    */
     for(int i=0;i<n;i++){
         heap[i]=v[i];
     }
@@ -33,14 +35,13 @@ void creat(int heap[],int v[],int n){
         down(heap,j-1,n);
     }
 }
-
 void up(int heap[],int j,int value, int n){
     /*
-    Função para aumentar prioridade elementos do heap.
-    heap[] -> heap criado.
-    j -> posição do elemento que irá descer. (0 até n-1)
-    value -> novo valor
-    n -> numero de elementos do vetor.
+    * Função para aumentar prioridade elementos do heap.
+    * heap[] -> heap criado.
+    * j -> posição do elemento que irá descer. (0 até n-1)
+    * value -> novo valor
+    * n -> numero de elementos do vetor.
     */
     int aux = j/2;
     heap[j] = value;
@@ -51,11 +52,10 @@ void up(int heap[],int j,int value, int n){
         up(heap,aux,value,n);
     }
 }
-
 void remove_element(int heap[],int *n){
     /*
-    heap[] -> heap criado.
-    n -> ponteiro do número de elementos do vetor.
+    * heap[] -> heap criado.
+    * n -> ponteiro do número de elementos do vetor.
     */
     heap[0] = heap[*n-1];
     heap[*n-1] = 0;
@@ -64,14 +64,12 @@ void remove_element(int heap[],int *n){
     heap=(int*)realloc(heap,sizeof(int)*aux);
     down(heap,0,*n);
 }
-
 void show(int heap[], int *n){
     printf("\nHEAP ( ");
     for(int i=0;i<*n;i++){
         printf("%d ",heap[i]);
     }printf(")\n");
 }
-
 void insert(int heap[], int *n,int value){
     *n+=1;
     int aux = *n;
@@ -80,7 +78,6 @@ void insert(int heap[], int *n,int value){
     show(heap, n);
     up(heap,aux-1,value,*n);
 }
-
 void heapsort(int vector[],int len){
     int vOut[len];
     int *h;
@@ -95,10 +92,9 @@ void heapsort(int vector[],int len){
         printf("%d ", vOut[i]);
     }printf("\n");
 }
-
 int main(){
     int v[] = {8, 23, 53, 41, 62, 36, 78, 7, 19, 10, 11, 67, 9, 123};
-    int n= sizeof(v)/sizeof(v[0]); //tamanho do vetor v
+    int n= sizeof(v)/sizeof(v[0]); //Tamanho do vetor v
     int *heap;
     heap = malloc(sizeof(heap)*n);
 
@@ -107,7 +103,7 @@ int main(){
     for(int i=0;i<n;i++)
         printf("%d ", v[i]);
     printf(")");
-    creat(heap,v,n);  //criando o heap
+    creat(heap,v,n);  //Criando heap com o vetor v
     show(heap,&n);
     
     printf("\nAlterando o valor do 8° elemento do heap para 172");
@@ -126,7 +122,7 @@ int main(){
     insert(heap,&n,99);
     show(heap,&n);
 
-    printf("\nImplementando o headsort");
+    printf("\nImplementando o heapsort");
     int vector[] = {8, 23, 53, 41, 62, 36, 78, 7, 19, 10, 11, 49, 89, 123,99,14,1};
     int len= sizeof(vector)/sizeof(vector[0]);
     heapsort(vector, len);
